560ea251ec4ee479644964d6483a458c140e8239
Shadan <shadanamini@gmail.com>
2022-08-07 06:41:51 -0700

    added time and location and added google fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
 
 function App() {
   return (
-  <div className="mx-auto max-w-screen-md mt-8 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-  <TopButtons />
-  <Inputs />  
-  </div>
+    <div className="mx-auto max-w-screen-md mt-8 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+      <TimeAndLocation />  
+    </div>
   )
 }
 

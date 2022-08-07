669a8857b1521db822997f04771e24051d269087
Shadan <shadanamini@gmail.com>
2022-08-07 05:29:46 -0700

    created initial container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+  )
 }
 
 export default App;

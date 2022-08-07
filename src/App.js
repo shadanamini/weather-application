7cec7b5405bbb774f76a127f51d9656ef402b1f9
Shadan <shadanamini@gmail.com>
2022-08-07 05:32:07 -0700

    adjusted container size

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
-  <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+  <div className="mx-auto max-w-screen-sm mt-8 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
   )
 }
 

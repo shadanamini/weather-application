d5983154c2908c228313c81b087501d5b36d50af
Shadan <shadanamini@gmail.com>
2022-08-07 05:48:47 -0700

    added top buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
 
 function App() {
   return (
-  <div className="mx-auto max-w-screen-sm mt-8 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+  <div className="mx-auto max-w-screen-md mt-8 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+  <TopButtons />  
+  </div>
   )
 }
 
